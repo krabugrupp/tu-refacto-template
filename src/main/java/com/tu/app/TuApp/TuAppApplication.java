c4c2c4e78c33ca3d505a21b90166bcd34b05fb35
Nikita Saprõkin <n.saprokin@gmail.com>
2024-02-19 11:43:06 +0200

    first commit

---
+++ src/main/java/com/tu/app/TuApp/TuAppApplication.java
@@ -0,0 +1,12 @@
+package com.tu.app.TuApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+@SpringBootApplication
+public class TuAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TuAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tu/app/TuApp/TuAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tu.app.TuApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TuAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
