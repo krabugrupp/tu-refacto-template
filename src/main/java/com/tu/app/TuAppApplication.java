d50fd8d395d84c08d9ba727a310ec873a58d3bac
Nikita Saprõkin <n.saprokin@gmail.com>
2024-02-19 18:46:13 +0200

    Swagger setted up

--- src/main/java/com/tu/app/TuAppApplication.java
+++ src/main/java/com/tu/app/TuAppApplication.java
@@ -1,9 +1,13 @@
 package com.tu.app;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@OpenAPIDefinition(info = @Info(title = "My API", version = "1.0", description = "API for something"))
 @SpringBootApplication
 public class TuAppApplication {
 	private static final Logger log = LoggerFactory.getLogger(TuAppApplication.class);
