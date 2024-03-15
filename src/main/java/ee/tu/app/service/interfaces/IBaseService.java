8bf390967b737759b0e8f24cfa09d469df58691a
Nikita Saprõkin <n.saprokin@gmail.com>
2024-03-15 17:16:24 +0200

    Interface for base service

--- src/main/java/ee/tu/app/service/interfaces/IBaseService.java
+++ src/main/java/ee/tu/app/service/interfaces/IBaseService.java
@@ -1,5 +1,20 @@
 package ee.tu.app.service.interfaces;
 
+import ee.tu.app.repo.IBaseRepo;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.Optional;
+
 public interface IBaseService <TClass>  {
 
+    public List<TClass> getAll();
+
+    public Optional<TClass> get(Long id);
+
+    public TClass update(TClass updated);
+
+    public TClass create(TClass newDomain);
+
+    public void delete(Long id);
 }
